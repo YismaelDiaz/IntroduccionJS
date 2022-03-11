dad24717347f5a2dd7017d14a3241a8ca51e3887
YismaelDiaz <yismandres@gmail.com>
2022-03-10 18:54:54 -0600

    FInal

---
+++ practica1.js
@@ -0,0 +1,20 @@
+var dato = Number (prompt("Ingresa un número"))
+dato = (dato*2)
+console.log(dato)
+
+
+
+var dato1 = Number (prompt("Ingresa un número"))
+var dato2 = Number (prompt("Ingresa otro número"))
+console.log(dato1-dato2)
+
+
+var radio=5.3
+var area= (3.1416*(radio**2))
+console.log(area)
+
+var D=8
+var d=6
+var area= ((d*D)/2)
+console.log(area)
+
